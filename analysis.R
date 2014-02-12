a903458846ae3aa8142295d2abfaf254b96b0641
Luke W Johnston <lwjohnst@gmail.com>
2014-02-11 20:32:08 -0500

    Included variable to return the version of R. Need to do that for all software used, to keep as a record

--- analysis.R
+++ analysis.R
@@ -1,7 +1,7 @@
-## This R script was created on $DATE by Luke Johnston.  The file
-## contains code to analyze data from ${DatasetName} for the project
-## ${ProjectDir}.  This script likely contains a large portion of code
-## for generating graphics, plots, and regression diagnostics.
+## This R ($RVERSION) script was created on $DATE by Luke Johnston.
+## The file contains code to analyze data from ${DatasetName} for the
+## project ${ProjectDir}.  This script likely contains a large portion
+## of code for generating graphics, plots, and regression diagnostics.
 ## However, it may also contain code for the main analysis for the
 ## project.
 
