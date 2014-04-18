9559a418573e854c8ef0a93ade0b3cd90f5be4a8
Luke W Johnston <lwjohnst@gmail.com>
2014-04-18 17:40:17 -0400

    Added a template Docear file

---
+++ templates/mindmap.mm
@@ -0,0 +1,99 @@
+<map version="docear 1.1" project="143B62ADFC35Y65VCR8T8B573UP5DTRMOGBE" project_last_home="file:/home/luke/Documents/bibliography/" dcr_id="1391619023018_ufhvsp36hp494l6hb42xzptk">
+<!--To view this file, download Docear - The Academic Literature Suite from http://www.docear.org -->
+<node TEXT="[Title of project]" FOLDED="false" ID="ID_1647181744" CREATED="1390338736217" MODIFIED="1397856379318">
+<hook NAME="AutomaticEdgeColor" COUNTER="6"/>
+<hook NAME="MapStyle">
+    <properties show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Plan of action" POSITION="right" ID="ID_454260268" CREATED="1391541923639" MODIFIED="1391625120593" MAX_WIDTH="400" MIN_WIDTH="1" MOVED="1391621972071">
+<edge COLOR="#ff0000"/>
+<node TEXT="Break each question down into its constituent parts" ID="ID_943567419" CREATED="1391544381997" MODIFIED="1391625164918" MAX_WIDTH="400" MIN_WIDTH="1"/>
+<node TEXT="Develop each question like it is a systematic&#xa;review and meta-analysis (as well as its parts)" ID="ID_1119924619" CREATED="1391541793921" MODIFIED="1391546472668" MAX_WIDTH="400" MIN_WIDTH="1">
+<icon BUILTIN="yes"/>
+</node>
+<node TEXT="The question&apos;s answer should be narrow and&#xa;focused, to prevent drifting in the literature&#xa;ocean." ID="ID_1706001724" CREATED="1391544209008" MODIFIED="1391546472715" MAX_WIDTH="400" MIN_WIDTH="1"/>
+<node TEXT="Develop search terms for each question" ID="ID_1087725076" CREATED="1391542007550" MODIFIED="1391546472762" MAX_WIDTH="400" MIN_WIDTH="1">
+<icon BUILTIN="yes"/>
+</node>
+<node TEXT="Obtain as many articles as possible&#xa;on first few search sessions" ID="ID_1848281300" CREATED="1391542151426" MODIFIED="1397856429537" MAX_WIDTH="400" MIN_WIDTH="1"/>
+<node TEXT="Go through the articles, starting from&#xa;the review articles, down to original&#xa;research articles, using highlight and&#xa;comments so Docear can track them." ID="ID_547223283" CREATED="1391543075138" MODIFIED="1391546472927" MAX_WIDTH="400" MIN_WIDTH="1"/>
+<node TEXT="As you read, keep in mind potential questions/issues&#xa;and store these potential questions in an additional&#xa;Docear branch (labeled appropriately)" ID="ID_1315245752" CREATED="1391543298504" MODIFIED="1397856477223" MAX_WIDTH="400" MIN_WIDTH="1">
+<icon BUILTIN="yes"/>
+</node>
+<node TEXT="Also, as you read, look into potential relevant&#xa;references within each article" ID="ID_1826121089" CREATED="1391543479380" MODIFIED="1391546472970" MAX_WIDTH="400" MIN_WIDTH="1"/>
+<node TEXT="After reading each article, develop a concept map of the relevant ideas/concepts" ID="ID_1347106559" CREATED="1397856500449" MODIFIED="1397857169399"/>
+</node>
+<node TEXT="Articles" POSITION="right" ID="ID_224592390" CREATED="1391643131784" MODIFIED="1391643136235">
+<edge COLOR="#00ff00"/>
+<node TEXT="Unread" ID="ID_1493107336" CREATED="1391643137225" MODIFIED="1391643138907">
+<node TEXT="Top priority" ID="ID_1220917785" CREATED="1397854924599" MODIFIED="1397854929337"/>
+<node TEXT="Lower priority" ID="ID_309971005" CREATED="1397854929529" MODIFIED="1397854939347"/>
+</node>
+<node TEXT="Read" ID="ID_362117540" CREATED="1391643139264" MODIFIED="1397854920493"/>
+</node>
+<node TEXT="Potential questions/issues" POSITION="right" ID="ID_997625618" CREATED="1397856447413" MODIFIED="1397856453032">
+<edge COLOR="#ffff00"/>
+</node>
+<node TEXT="Question(s)" POSITION="left" ID="ID_496149396" CREATED="1391643119496" MODIFIED="1391643161385">
+<edge COLOR="#ff0000"/>
+<node TEXT="[Research Question]" ID="ID_1067037046" CREATED="1397856100996" MODIFIED="1397856386288">
+<node TEXT="Focus down:" ID="ID_1851233913" CREATED="1397856136942" MODIFIED="1397856156590"/>
+<node TEXT="Search term(s):" ID="ID_1825557920" CREATED="1391643124064" MODIFIED="1397856169609" MOVED="1397856169617"/>
+<node TEXT="Date searched and hits:" ID="ID_1957981418" CREATED="1397856179899" MODIFIED="1397856185556"/>
+</node>
+</node>
+<node TEXT="Manuscript organization" POSITION="left" ID="ID_918697966" CREATED="1397856203807" MODIFIED="1397856210951">
+<edge COLOR="#ff00ff"/>
+</node>
+<node TEXT="Time allotment/schedule" POSITION="left" ID="ID_1458267807" CREATED="1397856235348" MODIFIED="1397856247476">
+<edge COLOR="#00ffff"/>
+</node>
+</node>
+</map>
