45a59657578bbdbbcf38aa7899fec755323e253a
Luke W Johnston <lwjohnst@gmail.com>
2014-10-22 13:06:17 -0400

    Major edits to the prodigen.sh script. Near complete overhaul

--- prodigen.sh
+++ prodigen.sh
@@ -21,34 +21,12 @@ read FILLEDCONF
 APP=$(readlink -f "$0")
 APPDIR=$(dirname $APP)
 TEMPLATES=$APPDIR/templates
-echo -e "$APP\n$APPDIR\n$TEMPLATES"
-
-## Set a shell variable with all of the template files and their
-## copied location (for example, the manuscript.md file in the .
-REP_DIR=./report
-SRC_DIR=./scripts
-LIT_DIR=./lit
-MM_DIR=$LIT_DIR/mindmap
-
-TMPLT_FILES="
-    ./README.md 
-    ./Makefile 
-    $REP_DIR/manuscript.md 
-    $SRC_DIR/*.sas 
-    $SRC_DIR/*.R 
-    $REP_DIR/options.tex 
-    $LIT_DIR/searchStrategy+Issues.md 
-    $MM_DIR/mindmap.mm
-"
-
 
 ## Check answer to FILLEDCONF and source the CONFIG_FILE
 CONFIG_FILE=$APPDIR/pdg.conf
 
-if [[ $FILLEDCONF = "yes" ]]
-then 
-    if [[ -f $CONFIG_FILE ]]
-    then
+if [[ $FILLEDCONF = "yes" ]]; then 
+    if [[ -f $CONFIG_FILE ]]; then
         echo $CONFIG_FILE
         . $CONFIG_FILE
     fi
@@ -60,68 +38,71 @@ fi
 ## Set the project base directory
 PROJ_BASE_DIR=$RESEARCHDIR/$PROJECT
 
+## Set a shell variable with all of the template files and their
+## copied location (for example, the manuscript.md file in the .
+REP_DIR=$PROJ_BASE_DIR/report
+SRC_DIR=$PROJ_BASE_DIR/scripts
+FUN_DIR=$SRC_DIR/functions
+LIT_DIR=$PROJ_BASE_DIR/lit
+MM_DIR=$LIT_DIR/mindmap
+GIT_DIR=$PROJ_BASE_DIR/.git
+
+TMPLT_FILES="
+    $PROJ_BASE_DIR/README.md 
+    $PROJ_BASE_DIR/Makefile 
+    $REP_DIR/manuscript.md 
+    $SRC_DIR/01-variables.sas 
+    $SRC_DIR/02-analysis.sas 
+    $SRC_DIR/03-plots.R 
+    $REP_DIR/options.tex 
+    $LIT_DIR/searchStrategy+Issues.md 
+    $MM_DIR/mindmap.mm
+"
+
 ## Check to make sure a folder doesn't already exist with the
 ## $PROJECT name
-
 if [[ -d $PROJ_BASE_DIR ]]
 then
     echo "Error: Folder already exists."
     exit 1 ## Causes script to exit with a failure
 else
-    mkdir -v $PROJECT
-    ## The "$_" = using the first argument of the previous command
-    #pwd
+    mkdir -v $PROJ_BASE_DIR
 fi
 
-exit 1
-
 ## Create main folders within research project folder
-for dir in ${DIRTREE[@]}
-do
-    mkdir -vp $dir
+for dir in ${DIRTREE[@]}; do
+    mkdir -vp $PROJ_BASE_DIR/$dir
 done
 
 ## Copy functions and macros to project, chmod to read-only (444).
 ## Only the master files can be edited, with these copies able to be
 ## updated using the makefile (make referesh)
-if [ ! -z "$FUNCTIONS" ]; then
-    for i in $FUNCTIONS $MACROS; do
-        cp -fvu $i scripts/functions
+if [ -n "$FUNCTIONS" -o -n "$MACROS" ]; then
+    for func in $FUNCTIONS $MACROS; do
+        cp -fvu $func $FUN_DIR/$(basename $func)
     done
 fi
-    chmod 444 ./scripts/functions/*.{sas}
+chmod 444 $FUN_DIR/*
 
 ## Copy the template files from the `Template` folder to the project
-for file in $TMPLT_FILES
-do
+for file in $TMPLT_FILES; do
     cp -fvu $TEMPLATES/$(basename $file) $file
 done
 
-for i in README.md Makefile; do 
-    cp -fvu $TEMPLATES/$i .
-done
-
-for i in manuscript.md options.tex; do
-    cp -fvu $TEMPLATES/$i report
-done
-
-for i in variables.sas analysis.sas plots.R; do
-    cp -fvu $TEMPLATES/$i scripts
-done
-
-cp -fvu $TEMPLATES/mindmap.mm lit/mindmap/mindmap.mm
-
-cat > TODO.org <<EOF
+## Create TODO and ideas.md files
+cat > $PROJ_BASE_DIR/TODO.org <<EOF
 * Task list
 
 EOF
 
-cat > ideas.md <<EOF
+cat > $PROJ_BASE_DIR/ideas.md <<EOF
 # Potential ideas for analysis #
 
 EOF
 
-find . -type f -exec  sed -i -e 's/\$PROJECT/'$PROJECT'/g' \
+## Change variable names throughout files to their proper names, as
+## determined in the pdf.conf file
+find $PROJ_BASE_DIR -type f -exec  sed -i -e 's/\$PROJECT/'$PROJECT'/g' \
     -e 's/\${PROJECT}/'$PROJECT'/g' \
     -e 's;\$BIBLIO;'$BIBLIO';g' \
     -e 's;\$BIBSTYLE;'$BIBSTYLE';g' \
@@ -138,30 +119,35 @@ find . -type f -exec  sed -i -e 's/\$PROJECT/'$PROJECT'/g' \
     {} \;
 
 ## Create a PDF of the README file.
-pandoc README.md -o README.pdf
+make -C $PROJ_BASE_DIR readme
 
 ## Output to screen the entire directory structure
-ls -R
+ls -R $PROJ_BASE_DIR
 
 ## Initiate a git repository.  Git is a version control system which
 ## allows tracking of changes made to the files and allows you to go
 ## back to previous versions of a file.  This is a very powerful and
 ## useful software for documenting what you did and why!
-git init
+git --git-dir=$GIT_DIR init
 
 ## Create a file to tell git to ignore version controlling the given
 ## files EOF stands for end of file. The << creates a here-document
 ## that outputs all the text between the two EOF and inputs it into
 ## the > ./filename
-cat > .gitignore <<EOF
+cat > $PROJ_BASE_DIR/.gitignore <<EOF
 ## This file tells git to ignore the following files and folders:
 *~
 .~*
 output/
+*.el
+*/auto/
+*.tex
+!options.tex
 EOF
 
 ## Add the files to be tracked by git
-git add .
+git --git-dir=$GIT_DIR --work-tree=$PROJ_BASE_DIR add .
 
 ## Commit the first version
-git commit -m "Project's first version commit."
+git --git-dir=$GIT_DIR --work-tree=$PROJ_BASE_DIR \
+    commit -m "Project's first version commit."
